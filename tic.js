5894c7d79378d59d13e6b1c76f0ad10f8eab7ac5
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-09 00:42:30 -0800

    added HTML, JS

---
+++ tic.js
@@ -0,0 +1,33 @@
+//make function called computerPlay//set parameter for computerPlay for return value
+function computerPlay() 
+{
+  let choice = Math.floor(Math.random() * 3);
+  
+  if (choice == 0) 
+  {
+    return "Rock";
+  } 
+  else if (choice == 1) 
+  {
+    return "Paper";
+  } 
+  else
+  {
+    return "Scissor";
+  }
+}
+
+console.log(computerPlay());
+}
+//return value of either rock, paper, scissor
+
+//declare function for single round of Rock Paper Scissors called playRound
+//allow functions to take 2 parameters (playerSelection) and (computerSelection)
+//return string that declares winner of round "You Lose! Paper beats Rock"
+    //player parameter needs to be case insensitive
+
+//declare a function called game().
+//call playRound inside 5 times
+//report winner/loser at end
+
+//prompt() user for input
