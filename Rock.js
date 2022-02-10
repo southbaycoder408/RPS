b5f7b2e03328d04523bfb191143317005bfa3c75
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-09 17:38:41 -0800

    added toLowerCase string for insensitive case input

--- Rock.js
+++ Rock.js
@@ -1,51 +1,49 @@
-function computerPlay() //make function called computerPlay//set parameter for computerPlay for return value
+function computerPlay() //make function called computerPlay and set parameter for computerPlay for return value
 {
   let choice = Math.floor(Math.random() * 3); //return value of either rock, paper, scissor
 
   if (choice == 0) {
-    return "Rock";
+    return "rock";
   } else if (choice == 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissor";
+    return "scissor";
   }
 }
-
-//declare function for single round of Rock Paper Scissors called playRound
-function playRound(playerSelection, computerSelection) {//allow functions to take 2 parameters (playerSelection) and (computerSelection)
+function playRound(playerSelection, computerSelection) {//declare function for single round of Rock Paper Scissors called playRound and allow functions to take 2 parameters (playerSelection) and (computerSelection)
 
 let userWins = "User Wins!!!"
 let userLose = "Computer Wins!!!"
 let userChoice = "User Choice: "
 let computerChoice = "Computer Choice: "
+
 console.log("User Choice: " + playerSelection);
 console.log("Computer Choice: " + computerSelection);
 
-
   if (playerSelection == computerSelection) {
     return alert("It's a Tie!!!");
-  } else if (playerSelection == "Rock" && computerSelection == "Scissor") {
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
     return alert(userWins);
-  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+  } else if (playerSelection == "rock" && computerSelection == "paper"){
     return alert(userLose);
-  } else if (playerSelection == "Paper" && computerSelection == "Scissor"){
+  } else if (playerSelection == "paper" && computerSelection == "scissor"){
     return alert(userLose);
-  } else if (playerSelection == "Paper" && computerSelection == "Rock"){
+  } else if (playerSelection == "paper" && computerSelection == "rock"){
     return alert(userWins);
-  } else if (playerSelection == "Scissor" && computerSelection == "Rock"){
+  } else if (playerSelection == "scissor" && computerSelection == "rock"){
     return alert(userLose);
-  } else if (playerSelection == "Scissor" && computerSelection == "Paper"){
+  } else if (playerSelection == "scissor" && computerSelection == "paper"){
     return alert(userWins);
   }
 
 }
 
 let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+playerSelection = playerSelection.toLowerCase();
 let computerSelection = computerPlay();
 
+
 playRound(playerSelection, computerSelection)
-
-
 
 //return string that declares winner of round "You Lose! Paper beats Rock"
 //player parameter needs to be case insensitive
