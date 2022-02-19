9e06f6ba1c9d4aa67b0819bd4b8a001724124e08
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-18 23:53:22 -0800

    fixed and updated function playround to running based off button input at bottom of scripts

--- Rock.js
+++ Rock.js
@@ -13,77 +13,89 @@ function computerPlay() //make function called computerPlay and set parameter fo
 
 function playRound(playerSelection, computerSelection) { //declare function for single round of Rock Paper Scissors called playRound and allow functions to take 2 parameters (playerSelection) and (computerSelection)
   
- // const btn = document.querySelector('#btn1');
- // btn.addEventListener('click', () => {
- //   alert("Hello World");
-//  });
-
-
-  let userWins = "You win!"; //return string that declares winner of round "You Lose! Paper beats Rock"
-  let userLose = "You lose!"; //return string that declares winner of round "You Lose! Paper beats Rock"
-  let tie = "It's a Tie!";
-  let userChoice = "User: ";
-  let computerChoice = "Computer: "
-
-  console.log("     You played " + playerSelection);
-  console.log("Computer played " + computerSelection);
-
+  console.log("You played " + playerSelection + " while the Computer played " + computerSelection + ".");
+  playerSelection = playerSelection.toLowerCase();
+  
   if (playerSelection == computerSelection) {
-    return tie;
+    console.log("It's a Tie!");
+    return;
   } else if (playerSelection == "rock" && computerSelection == "scissor") {
-    return userWins;
+    console.log("You win!");
+    return;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return userLose;
+    console.log("You lose!");
+    return;
   } else if (playerSelection == "paper" && computerSelection == "scissor") {
-    return userLose;
+    console.log("You lose!");
+    return;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return userWins;
+    console.log("You win!");
+    return;
   } else if (playerSelection == "scissor" && computerSelection == "rock") {
-    return userLose;
+    console.log("You lose!");
+    return;
   } else if (playerSelection == "scissor" && computerSelection == "paper") {
-    return userWins;
+    console.log("You win!");
+    return;
+  } else{
+    console.log("ruh roh, a boo boo made");
+    return;
   }
 }
 
-function game(){
-  let win = "You win!";
-  let lose = "You lose!";
-  let tie = "It's a Tie!";
-  let playerScore = 0;
-  let computerScore =0;
-  let tieScore = 0;
+// function game(){
+//   let win = "You win!";
+//   let lose = "You lose!";
+//   let tie = "It's a Tie!";
+//   let playerScore = 0;
+//   let computerScore =0;
+//   let tieScore = 0;
 
-  // for (let i = 0; i < 5; i++) {
+//   for (let i = 0; i < 5; i++) {
 
-  //   let playerSelection = prompt("Choose Rock, Paper, or Scissors");
-  //   playerSelection = playerSelection.toLowerCase(); //player parameter needs to be case insensitive
-  //   let computerSelection = computerPlay();
-  //   let score = playRound(playerSelection, computerSelection);
+//     let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+//     playerSelection = playerSelection.toLowerCase(); //player parameter needs to be case insensitive
+//     let computerSelection = computerPlay();
+//     let score = playRound(playerSelection, computerSelection);
 
-  //   if(score == win)
-  //   {
-  //     ++playerScore;
-  //   }
-  //   else if(score == lose){
-  //     ++computerScore;
-  //   }
-  //   else if(score == tie){
-  //     ++tieScore;
-  //   }
-  // }
-  console.log("TOTAL SCORE");
-  console.log("Player Points: " + playerScore);
-  console.log("Computer Points: " + computerScore);
-  console.log("Tie Points: " + tieScore);
+//     if(score == win)
+//     {
+//       ++playerScore;
+//     }
+//     else if(score == lose){
+//       ++computerScore;
+//     }
+//     else if(score == tie){
+//       ++tieScore;
+//     }
+//   }
+//   console.log("TOTAL SCORE");
+//   console.log("Player Points: " + playerScore);
+//   console.log("Computer Points: " + computerScore);
+//   console.log("Tie Points: " + tieScore);
 
-  if(playerScore < computerScore){
-    alert("SHAMEFUL");
-  }
-  else if(playerScore > computerScore){
-    alert("VICTORIOUS");
-  }
-  else if(playerScore == computerScore){
-    alert("Meh...");
-  }
+//   if(playerScore < computerScore){
+//     alert("SHAMEFUL");
+//   }
+//   else if(playerScore > computerScore){
+//     alert("VICTORIOUS");
+//   }
+//   else if(playerScore == computerScore){
+//     alert("Meh...");
+//   }
 
-}
+// }
+
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+btn1.addEventListener('click', () => {
+playRound("Rock",computerPlay());  
+});
+btn2.addEventListener('click', () => {
+playRound("Paper",computerPlay());  
+});
+btn3.addEventListener('click', () => {
+playRound("Scissor",computerPlay());  
+});
