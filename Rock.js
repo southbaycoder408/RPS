f97ddb37d0743210e1d7591e7ef8b664225ef2f0
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-21 01:18:45 -0800

    code functional now, need to refine at some point

--- Rock.js
+++ Rock.js
@@ -1,16 +1,26 @@
 const container = document.querySelector('#container');
 const result = document.createElement('p');
 const score = document.createElement('p');
-result.classList.add('outcome');
-score.classList.add('scoreCount');
-container.appendChild(score);
-container.appendChild(result);
-
+const matchResults = document.createElement('p')
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((button => { 
+result.classList.add('roundOutcome');
+score.classList.add('scoreCount');
+matchResults.classList.add('matchResults');
+
+// container.appendChild(score);
+// container.appendChild(result);
+
+let roundResult;
+let scoreTrack;
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+buttons.forEach((button => { //this creates an evenlistener button for each choice for rock paper scissors
   button.addEventListener('click', () => {
-    playRound(button.id, computerPlay());
+    roundResult = playRound(button.id, computerPlay());
+    scoreTrack = checkWinCondition();
   })
 }));
 
@@ -32,62 +42,53 @@ function playRound(playerSelection, computerSelection) { //declare function for 
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection == computerSelection) {
+    const outcome = "tie";
     result.textContent = "You played " + playerSelection + " while the Computer played " + computerSelection + ". Oh...it's a tie.";
     container.appendChild(result);
-    return;
+    return outcome;
   } else if ((playerSelection == "rock" && computerSelection == "scissor") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissor" && computerSelection == "paper")) {
+    const outcome = "win";
     result.textContent = "You played " + playerSelection + " while the Computer played " + computerSelection + ". You won this one!!";
     container.appendChild(result);
-    return;
+    return outcome;
   } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissor") || (playerSelection == "scissor" && computerSelection == "rock")) {
+    const outcome = "lose";
     result.textContent = "You played " + playerSelection + " while the Computer played " + computerSelection + ". Darn you lost this round!";
     container.appendChild(result);
-    return;
+    return outcome;
   } else {
     alert("ruh roh, I made a boo boo");
     return;
   }
 }
 
-// function game(){
-//   let win = "You win!";
-//   let lose = "You lose!";
-//   let tie = "It's a Tie!";
-//   let playerScore = 0;
-//   let computerScore =0;
-//   let tieScore = 0;
+function checkWinCondition() {
+  if(playerScore !== 5 && computerScore !== 5){
+    if (roundResult == "win") {
+      playerScore++;
+      score.textContent = "Player Score : " + playerScore + ", Computer Score : " + computerScore + ", and Ties: " + tieScore;
+      container.appendChild(score);
+    } else if (roundResult == "lose") {
+      computerScore++;
+      score.textContent = "Player Score : " + playerScore + ", Computer Score : " + computerScore + ", and Ties: " + tieScore; 
+      container.appendChild(score);
+    } else if (roundResult == "tie") {
+      tieScore++;
+      score.textContent = "Player Score : " + playerScore + ", Computer Score : " + computerScore + ", and Ties: " + tieScore;
+      container.appendChild(score);
+    } else if (typeof roundResult != 'string') {
+      alert("woops a boo boo");
+    }
+  }
 
-//   for (let i = 0; i < 5; i++) {
-
-//     let playerSelection = prompt("Choose Rock, Paper, or Scissors");
-//     playerSelection = playerSelection.toLowerCase(); //player parameter needs to be case insensitive
-//     let computerSelection = computerPlay();
-//     let score = playRound(playerSelection, computerSelection);
-
-//     if(score == win)
-//     {
-//       ++playerScore;
-//     }
-//     else if(score == lose){
-//       ++computerScore;
-//     }
-//     else if(score == tie){
-//       ++tieScore;
-//     }
-//   }
-//   console.log("TOTAL SCORE");
-//   console.log("Player Points: " + playerScore);
-//   console.log("Computer Points: " + computerScore);
-//   console.log("Tie Points: " + tieScore);
-
-//   if(playerScore < computerScore){
-//     alert("SHAMEFUL");
-//   }
-//   else if(playerScore > computerScore){
-//     alert("VICTORIOUS");
-//   }
-//   else if(playerScore == computerScore){
-//     alert("Meh...");
-//   }
-
-// }
+  if(playerScore === 5){
+    matchResults.textContent = "VICTORY!!";
+    container.appendChild(matchResults);
+  }
+  else if(computerScore === 5){
+    matchResults.textContent = "DEFEAT!!"
+    container.appendChild(matchResults);
+  }
+  else;
+  
+}
